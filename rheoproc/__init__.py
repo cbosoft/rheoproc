8e0b7e40f79b5a750084527dcc1c9728300167f7
Chris <chrismjboyle@gmail.com>
2020-03-02 11:03:37 +0000

    Added basic arg parsing in __init__.py

--- rheoproc/__init__.py
+++ rheoproc/__init__.py
@@ -1,7 +1,13 @@
 import sys
+
+from rheoproc.usage import show_usage_and_exit
+
+if '--help' or '-h' in sys.argv:
+    show_usage_and_exit()
+
 major, minor, micro, rel, serial = sys.version_info
 if major < 3 or minor < 8:
-    raise Exception('rheoproc needs at least Python 3.8 (Got: Python {0}.{1})'.format(major, minor))
+    raise Exception('tdlib needs at least Python 3.8 (Got: Python {0}.{1})'.format(major, minor))
 
 import importlib
 def __check_module(name):
@@ -12,7 +18,12 @@ def __check_module(name):
         return False
 
 __modules = ['numpy', 'matplotlib', 'sympy', 'lmfit', 'scipy']
-__missing_modules = [__module for __module in __modules if not __check_module(__module)]
 
-if __missing_modules:
-    raise ImportError(f"Some required modules are missing: {' '.join(__missing_modules)}.")
+__exit = False
+for __module in __modules:
+    if not __check_module(__module):
+        print(f'Could not import {__module}, ensure it is installed.')
+        __exit = True
+
+if __exit:
+    sys.exit(1)
