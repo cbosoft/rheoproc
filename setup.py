549958524a348c74e187367cfa772bbe685c5c2c
Chris <chrismjboyle@gmail.com>
2020-03-02 16:44:29 +0000

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,14 @@ def main():
         description='Rheometer log processing library',
         version='0.2', 
         packages=['rheoproc'],
+        install_requires=[
+            'numpy',
+            'scipy',
+            'sympy',
+            'matplotlib',
+            'lmfit',
+            'opencv-python'
+        ],
         ext_modules=[
             Extension('rheoproc.accelproc', accelproc_sources)
         ]
