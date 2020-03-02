75a12fb486105fd854056095cf0ca5f1bc7d81e8
Chris <chrismjboyle@gmail.com>
2020-03-02 16:27:20 +0000

    Updated python peakdet wrapper

--- rheoproc/peakdet.py
+++ rheoproc/peakdet.py
@@ -1,7 +1,7 @@
-from tdlib.accelproc import peak_detect
+from rheoproc.accelproc import peak_detect
 
-def get_peaks(t, v, threshold=0.0015):
+def get_peaks(t, v, threshold=0.0015, negative_peaks=False):
     '''
     Given a signal, obtain a list of where the peaks occur.
     '''
-    return peak_detect(list(t), list(v), threshold)
+    return peak_detect(list(t), list(v), threshold, negative_peaks)
