5ed7ad34eeed42c299f31f55a3fe3fd5915b29d7
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-03-24 11:03:02 +0000

    Remove "CLEAR" from print funcs: messes with progress bar

--- rheoproc/error.py
+++ rheoproc/error.py
@@ -4,8 +4,8 @@ from rheoproc.colours import *
 
 
 def timestamp(*args, **kwargs):
-    print(f'  {CLEAR}{DIM}{datetime.now().strftime("(%x %X.%f)")}{RESET}', *args, **kwargs)
+    print(f'\r  {DIM}{datetime.now().strftime("(%x %X.%f)")}{RESET}', *args, **kwargs)
 
 
 def warning(*args, **kwargs):
-    print(f'  {CLEAR}{FG_YELLOW}{datetime.now().strftime("(%x %X.%f)")}{RESET}', *args, **kwargs)
+    print(f'\r  {FG_YELLOW}{datetime.now().strftime("(%x %X.%f)")}{RESET}', *args, **kwargs)
