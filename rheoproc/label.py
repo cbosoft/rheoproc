4c14f36e3210018a30c710bd0e0c2fdcbd910c0c
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-06-11 18:03:13 +0100

    Adds labelling module

---
+++ rheoproc/label.py
@@ -0,0 +1,21 @@
+def fmt_lbl(label, symbol, unit):
+    lbl = label
+    if symbol or unit:
+        lbl += ', $'
+    if symbol:
+        lbl += f'{symbol}'
+    if unit:
+        lbl += f'\\rm/{unit}'
+    if symbol or unit:
+        lbl += '$'
+    return lbl
+
+
+def strainrate_label(units=r's^{-1}'):
+    return fmt_lbl('Strain rate', '\\dot\\gamma', units)
+
+def stress_label(units='Pa'):
+    return fmt_lbl('Stress', '\\sigma', units)
+
+def viscosity_label(units=r'Pa\,s'):
+    return fmt_lbl('Viscosity', '\\mu', units)
