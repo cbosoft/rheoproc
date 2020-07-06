e4b8387421da62ce70b8593b3f171ae1496ada9a
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-06 18:27:50 +0100

    Check for gcc versions, and revert to default if no local vers found

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from glob import glob
 import os
+import re
 from distutils.core import setup, Extension
 from importlib.util import spec_from_file_location, module_from_spec
 
 with open("rheoproc/version.py") as f:
     ver = f.readline().split('=')[-1].strip().replace('\'', '').replace('"', '')
 
-os.environ["CC"] = "gcc-10"
+
+def reglob(directory, pattern):
+    contents = os.listdir(directory)
+    return [c for c in contents if re.match(pattern, c)]
+
+def get_cc():
+    brewgcc = list(sorted(reglob('/usr/local/bin', 'gcc-\\d+')))
+    if not brewgcc:
+        return 'gcc'
+    return brewgcc[-1]
+
+
+# Find latest GNU gcc installed by homebrew
+os.environ["CC"] = get_cc()
 
 def main():
     accelproc_sources = glob('src/*.c')
