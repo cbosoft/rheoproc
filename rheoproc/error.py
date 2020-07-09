d248fc127a7c64531241a63d278891c499d334a5
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-09 18:13:38 +0100

    Check if interactive. If not, don't show messages.

--- rheoproc/error.py
+++ rheoproc/error.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from rheoproc.colours import *
 
+import __main__ as main
+interactivep = hasattr(main, '__file__')
 
 def timestamp(*args, **kwargs):
     print(f'\r  {DIM}{datetime.now().strftime("(%x %X.%f)")}{RESET}', *args, **kwargs)
@@ -9,3 +11,10 @@ def timestamp(*args, **kwargs):
 
 def warning(*args, **kwargs):
     print(f'\r  {FG_YELLOW}{datetime.now().strftime("(%x %X.%f)")}{RESET}', *args, **kwargs)
+
+def do_nothing(*args, **kwargs):
+    return
+
+if not interactivep:
+    timestamp = do_nothing
+    warning = do_nothing
