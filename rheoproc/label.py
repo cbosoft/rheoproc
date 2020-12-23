0aacda804cf6e2c5775a14e08219bf31cd5004d1
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-23 12:22:28 +0000

    Add label for strain

--- rheoproc/label.py
+++ rheoproc/label.py
@@ -14,20 +14,30 @@ def fmt_lbl(label, symbol, unit):
 def strainrate_label(units=r's^{-1}'):
     return fmt_lbl('Strain rate', '\\dot\\gamma', units)
 
+
+def strain_label(units=r'(unitless)'):
+    return fmt_lbl('Strain', '\\gamma', units)
+
+
 def stress_label(units='Pa'):
     return fmt_lbl('Stress', '\\sigma', units)
+
 
 def viscosity_label(units=r'Pa\,s'):
     return fmt_lbl('Viscosity', '\\mu', units)
 
+
 def temperature_label(units='K'):
     return fmt_lbl('Temperature', 'T', units)
+
 
 def time_label(units='s'):
     return fmt_lbl('Time', 't', units)
 
+
 def freq_label(units='Hz', symbol='\\omega'):
     return fmt_lbl('Frequency', symbol, units)
+
 
 def ffty_label(units=None, symbol=None):
     # TODO
