ad5e833be6000d4216adf5ebc82975aa09c204a4
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-29 22:20:12 +0000

    ProgressBar takes in func to display info on progress bar

--- rheoproc/progress.py
+++ rheoproc/progress.py
@@ -5,11 +5,12 @@ from rheoproc.error import __print as raw_print, timestamp
 
 class ProgressBar:
 
-    def __init__(self, length):
+    def __init__(self, length, info_func=None):
         self.length = length
         self.pos = -1
         self.lock = Lock()
         self.columns = int(os.popen('stty size', 'r').read().split()[1])
+        self.info_func = info_func
 
     def update(self, i=None):
 
@@ -22,13 +23,15 @@ class ProgressBar:
             i = self.pos + 1
 
         frac = f'▌{i+1}/{self.length}▐'
-        timestamp_ex = "  (10/30/19 15:57:02.659921) "
-        columns = self.columns - len(frac) - len(timestamp_ex)
+        if self.info_func:
+            info = self.info_func(self.length, i)
+            frac = f'▌{info}▐' + frac
+        columns = self.columns - len(frac)
         col_per_i = columns / self.length
         prog = int(col_per_i * (i+1))
         prog = frac.rjust(prog-1, '█') + '█'
         end = '' if i < self.length-1 else '\n'
-        timestamp(f'█{prog}\r', end=end)
+        print(f'█{prog}\r', end=end)
 
         self.pos = i
         self.lock.release()
