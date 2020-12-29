ab73fc998a89fbd670fc2baa28a30b09e55ffcb9
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-29 17:46:07 +0000

    Fix client conn reset bug
    Previously assumed buffer would always be full unless end of file: this
    is not the case if packets are dropped (i.e. in real applications).

--- rheoproc/client.py
+++ rheoproc/client.py
@@ -6,20 +6,19 @@ from rheoproc.port import PORT
 def get_from_server(server_addr, *args, **kwargs):
     data = (args, kwargs)
     data_encoded = pickle.dumps(data)
-    print(f'sending data ({len(data_encoded)} bytes) to server at {server_addr}:{PORT}')
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect( (server_addr, PORT) )
+        print(f'Sending query to server at {server_addr}:{PORT}')
         s.sendall(data_encoded)
 
+        print(f'Receiving result...')
         data = bytearray()
-        BUFFLEN = 1024
+        BUFFLEN = 4096
         while part := s.recv(BUFFLEN):
             data.extend(part)
-            if len(part) < BUFFLEN:
-                break
 
-        print(f'got {len(data)/1024/1024:.5f} MB back')
+    print(f'got {len(data)/1024/1024:.5f} MB back')
 
     data = pickle.loads(data)
     return data
