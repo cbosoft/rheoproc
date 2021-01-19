73ff1e8b84f1e37ef9c497ef779654b443c36cd3
Christopher Boyle <chrismjboyle@gmail.com>
2021-01-19 13:07:18 +0000

    Add named hardware version for when the new loadcell was added

--- rheoproc/hardware_versions.py
+++ rheoproc/hardware_versions.py
@@ -8,5 +8,6 @@
 HW_VER_PND_MONO = 20191209 # added pnd: data is combined into a single channel
 HW_VER_PND_SPLIT = 20200109 # pnd channel split to positive and negative. work needs to be done to recombine.
 HW_VER_PND_FILT = 20200110 # adc decoupling capacitor added
+HW_VER_NEWLC = 20200908 # changed to low-range LC
 
 # TODO document remaining hardware versions
